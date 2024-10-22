b5dfa36fe51180f598284c9fb88569eb7c9a3e2f
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-10-23 01:36:01 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $SOURCE_CODE ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Singhsawan/Grey-converter.git /Grey-converter
+else
+  echo "Cloning Custom Repo from $SOURCE_CODE "
+  git clone $SOURCE_CODE /Grey-converter
+fi
+cd /Grey-converter
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
